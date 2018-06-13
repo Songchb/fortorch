9dc6570598a50f22fa950665da720ef00e3b02bb
송찬범 <songchb@naver.com>
2018-06-13 17:53:55 +0900

    forst commit

---
+++ test02.py
@@ -0,0 +1,78 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.models as models
+import torch.optim as optim
+
+# for image
+import matplotlib.pyplot as plt
+import numpy as np
+
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+
+
+net = models.resnet18() # torchvision 에 이미 정의된 모델을 가져옵니다.net = models.
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+print('\n===> Training Start')
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+net.to(device)
+if torch.cuda.device_count() > 1:
+    print('\n===> Training on GPU!')
+    net = nn.DataParallel(net)
+
+epochs = 2  # dataset을 여러번 사용해 트레이닝을 시킵니다.
+
+for epoch in range(epochs):
+    print('\n===> epoch %d' % epoch)
+    running_loss = 0.0
+
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+
+        if i % 2000 == 1999:  # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1) # prediction
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i]))
+
